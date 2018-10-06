212d9cd26a9309f44dbdad1a0ff7643881444331
Matvey Malatsion <matvey@warefly.com>
2018-10-06 20:49:33 +0300

    Initial Commit

---
+++ Tinkoff Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinkoff Test
+//
+//  Created by Матвей Малацион on 06/10/2018.
+//  Copyright © 2018 mmalatsion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
